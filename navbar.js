4ed0eb2d95c1ee251b645f6145d8e4cef2492a4e
ekp-1 <138497015+ekpk-1@users.noreply.github.com>
2025-02-27 21:52:36 +1100

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -2,14 +2,9 @@ const navbar = document.querySelector('.floating-navbar');
 
 window.addEventListener('scroll', () => {
     if (window.scrollY > 500) {
-        navbar.style.display = 'flex'; // First set display to flex
-        setTimeout(() => {
-            navbar.classList.add('scrolled'); // Then add the scrolled class
-        }, 10);
+        navbar.classList.add('scrolled');
     } else {
         navbar.classList.remove('scrolled');
-        setTimeout(() => {
-            navbar.style.display = 'none';
-        }, 300); // Wait for transition to complete
     }
 });
+
